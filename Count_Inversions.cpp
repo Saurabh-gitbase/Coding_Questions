8bd64a6fb5a1d28fcee20045488620f33deb2f94
Saurabh <49409857+starkpa@users.noreply.github.com>
2020-04-01 22:05:44 +0530

    Update Count_Inversions.cpp

--- Count_Inversions.cpp
+++ Count_Inversions.cpp
@@ -1,3 +1,11 @@
+/*
+Given an array of positive integers. The task is to find inversion count of array.
+
+Inversion Count : For an array, inversion count indicates how far (or close) the array is from being sorted. If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count is the maximum. 
+Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -61,8 +69,9 @@ long int mergesort(int arr[],int n)
 
 int main()
  {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    	ios_base::sync_with_stdio(false);
+    	cin.tie(NULL);
+	
 	int t;
 	cin>>t;
 	while(t--)
