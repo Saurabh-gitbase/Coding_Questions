4f48139b0a8c892b86cc6752678e143936ae6e02
Saurabh <49409857+starkpa@users.noreply.github.com>
2020-04-06 23:27:42 +0530

    Update Minimum_Platforms.cpp

--- Minimum_Platforms.cpp
+++ Minimum_Platforms.cpp
@@ -1,3 +1,40 @@
 /*Given arrival and departure times of all trains that reach a railway station. Your task is to find the minimum number of platforms required for the railway station so that no train waits.*/
 
-#incl
+#include<bits/stdc++.h>
+using namespace std;
+
+int minPlatform(int a[],int d[],int n)
+{
+  multimap<int,char> data;
+  for(int i=0;i<n;i++)
+  {
+    data.make_pair(a[i],'a');
+  }
+  for(int i=0;i<n;i++)
+  {
+    data.make_pair(d[i],'d');
+  }
+  
+  int curr_count = 0;
+  int max_count = 0;
+  
+  multimap<int,char> :: iterator it = data.begin();
+  
+  for(;it!=data.end();it++)
+  {
+    if(it->second == 'a')
+      curr_count++;
+    else
+      curr_count--;
+    
+    if(curr_count>max_count)
+      max_count = curr_count;
+  }
+  
+  return max_count;
+}
+  
+
+int main()
+{
+}
