d962b9b8e01e30468b6975a37886283b971eaa83
Saurabh <49409857+starkpa@users.noreply.github.com>
2020-04-06 23:21:46 +0530

    Create Minimum_Platforms.cpp

---
+++ Minimum_Platforms.cpp
@@ -0,0 +1,3 @@
+/*Given arrival and departure times of all trains that reach a railway station. Your task is to find the minimum number of platforms required for the railway station so that no train waits.*/
+
+#incl
